6f3ec3cd02591f943f7a723096a3c202d5bbadce
jerrylai <jerrylai@aliyun.com>
2022-03-03 11:19:14 +0800

    add async

--- src/Assembly.cs
+++ src/Assembly.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © mycsv 2018")]
 [assembly: AssemblyTrademark("jerrylai@aliyun.com")]
 
-[assembly: AssemblyVersion("13.2.5")]
-[assembly: AssemblyFileVersion("13.2.5")]
+[assembly: AssemblyVersion("13.3.1")]
+[assembly: AssemblyFileVersion("13.3.1")]
--- version.cmd
+++ version.cmd
@@ -1,3 +1,3 @@
 @echo off
-set v=13.2.5
+set v=13.3.1
 tool\EditVersion dir="%cd%" v=%v% a="Assembly.cs"
