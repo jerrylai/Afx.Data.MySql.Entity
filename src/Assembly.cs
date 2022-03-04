1b5a53a9719cfdbc548d395e236cde22815251e6
jerrylai <jerrylai@aliyun.com>
2022-03-04 09:30:58 +0800

    13.3.3

--- src/Assembly.cs
+++ src/Assembly.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © mycsv 2018")]
 [assembly: AssemblyTrademark("jerrylai@aliyun.com")]
 
-[assembly: AssemblyVersion("13.3.1")]
-[assembly: AssemblyFileVersion("13.3.1")]
+[assembly: AssemblyVersion("13.3.3")]
+[assembly: AssemblyFileVersion("13.3.3")]
--- version.cmd
+++ version.cmd
@@ -1,3 +1,3 @@
 @echo off
-set v=13.3.1
+set v=13.3.3
 tool\EditVersion dir="%cd%" v=%v% a="Assembly.cs"
